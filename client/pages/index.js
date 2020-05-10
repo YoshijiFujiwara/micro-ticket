1399c21143c1cefae2edec805114d0aa2589089d
= <=>
2020-05-10 17:43:15 +0900

    get intial props

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,8 +1,30 @@
-export default () => {
-  return (
-    <>
-      <h1 class="text-danger">Landing pagess</h1>
-      <button class="btn btn-primary">送信</button>
-    </>
-  );
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+
+  return <h1>Landing Page</h1>;
 };
+
+LandingPage.getInitialProps = async ({ req }) => {
+  console.log(req.headers);
+  if (typeof window === "undefined") {
+    // on the server
+    // kubectl get service -n ingress-nginx のLoadBalancerの名前を参照
+    // headerを指定しないと動かないの、ちょっと難しいな
+    const { data } = await axios.get(
+      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+      {
+        headers: req.headers, // ホスト名やcookie情報が含まれるのでOK
+      }
+    );
+    return data;
+  } else {
+    // on the cliente
+    const { data } = await axios.get("/api/users/currentuser");
+    return data;
+  }
+  return {};
+};
+
+export default LandingPage;
