3b662f90f799d71241cc5b0ec587dcdead227620
= <=>
2020-05-10 23:12:40 +0900

    ticket model

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -20,8 +20,51 @@ it("return s status other than 401 if the user is signed in", async () => {
   expect(response.status).not.toEqual(401);
 });
 
-it("returns an error if an invalid title is provided", async () => {});
+it("returns an error if an invalid title is provided", async () => {
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title: "",
+      price: 1000,
+    })
+    .expect(400);
 
-it("returns an error if an invalid price is provided", async () => {});
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      price: 1000,
+    })
+    .expect(400);
+});
 
-it("create a ticket with valid inputs", async () => {});
+it("returns an error if an invalid price is provided", async () => {
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title: "hogehoge",
+      price: -1000,
+    })
+    .expect(400);
+
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title: "hogehoge",
+    })
+    .expect(400);
+});
+
+it("create a ticket with valid inputs", async () => {
+  // add in a check to make sure a ticket was saved
+  await request(app)
+    .post("/api/tickets")
+    .send({
+      title: "hogehoge",
+      price: 1000,
+    })
+    .expect(201);
+});
